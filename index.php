c044bae970a8c63fef36eb769a89aa73494b3b2f
Shane Gastmans <r0785896@student.thomasmore.be>
2021-08-08 20:01:18 +0200

    init

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <title>P&D bouw vorderingstaat</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <style>
+       #excel_area td:hover {background-color:green;color:white;}
+       .selectable {background-color:red;color:white;}
+       .selected {background-color:green;color:white;}
+       #fields {
+          display: none;
+         }
+         #load_excel_form {
+           position: sticky;
+           top:15px;
+           background-color: white;
+           padding: 7px;
+         }
+         .container {
+           max-height: 100%;
+         }
+         
+         header {
+           background-color: #c1001f;
+           width: 100%;
+           margin: 0;
+           padding: 0;
+         }
+
+         header img {
+           height: 90px;
+         }
+
+
+     </style>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+   </head>
+   <body>
+     <header>
+         <img src="/images/logo-small.jpg" alt="">
+     </header>
+     <div class="container">
+     <form method="post" id="load_excel_form" enctype="multipart/form-data">
+            <table id="filetable" class="table">
+              <tr>
+                <td width="25%" align="right">Selecteer Excel bestand</td>
+                <td width="25%" align="center"><input type="file" name="select_excel" /></td>
+		        <td width="25%" align="left"><input type="text" name="sheet" /></td>
+              </tr>
+            </table>
+            <input style="display:block;margin:auto;" type="submit" name="load" value="Upload excel bestand" width="100%" id="submitExcel" class="btn btn-primary" disabled />
+              <div id="fields">
+                  <label for="artnummer">Artnummer:</label>
+                  <input type="text" id="artnummer" name="artnummer" class="selectable" value="" readonly>
+                  <label for="eenheidsprijs">Beschrijving:</label>
+                  <input type="text" id="beschrijving" name="beschrijving" class="selectable" value="" readonly>
+                  <label for="eenheid">Eenheid:</label>
+                  <input type="text" id="eenheid" name="eenheid" class="selectable" value="" readonly>
+                  <!-- <<br> -->
+                  <label for="eenheidsprijs">Eenheidsprijs:</label>
+                  <input type="text" id="eenheidsprijs" name="eenheidsprijs" class="selectable" value="" readonly>
+                  <label for="hoeveelheid">Hoeveelheid:</label>
+                  <input type="text" id="hoeveelheid" name="hoeveelheid" class="selectable" value="" readonly>
+                  <label for="totaal">Totaal:</label>
+                  <input type="text" id="totaal" name="totaal" class="selectable" value="" readonly>
+                  <input type="submit">
+              </div>
+	   </form>
+
+      <br />
+      <br />
+      <div class="table-responsive">
+       <span id="message"></span>
+          
+       <br />
+          
+        <div id="excel_area"></div>
+      
+      </div>
+     </div>
+    </div>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  </body>
+</html>
+<script>
+$(document).ready(function(){
+  $('#load_excel_form').on('submit', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:"upload.php",
+            method:"POST",
+            data:new FormData(this),
+            contentType:false,
+            cache:false,
+            processData:false,
+            success:function(data)
+            {
+                if ($("#hoeveelheid").val() == "") {
+                    $("#filetable").hide();
+                    $("#submitExcel").hide();
+                    $('#excel_area').html(data);
+                    $('table').css('width','100%');
+                    $("#excel_area td").on("click", function(){
+                        $(".selected").val($(this).attr("class").split(" ")[0].match(/\d+/g) + "-" + $(this).closest("tr").attr("class").match(/\d+/g));
+                        let next = $(".selected").next().next();
+                        $(".selected").removeClass("selected");
+                        next.addClass("selected");
+                    });
+                    $("#fields").show();
+                  
+                } else {
+                    $("#message").html(data);
+                }
+            }
+        })
+    });
+
+  $(".selectable").on("click", function(){
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+  });
+  
+  $("#load_excel_form").on("change", function() {
+      $("#submitExcel").attr("disabled", false)
+  });
+
+});
+
+document.getElementById("load_excel_form").reset()
+
+</script>
---
+++ upload.php
@@ -0,0 +1,146 @@
+<?php
+
+//upload.php
+
+include 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+if($_FILES["select_excel"]["name"] != "") {
+    if ($_POST["beschrijving"] != "") {
+
+        [$artnummerCol, $artnummerRow] = explode("-", $_POST["artnummer"]);
+        [$beschrijvingCol, $beschrijvingRow] = explode("-", $_POST["beschrijving"]);
+        [$hoeveelheidCol, $hoeveelheidRow] = explode("-", $_POST["hoeveelheid"]);
+        [$eenheidCol, $eenheidRow] = explode("-", $_POST["eenheid"]);
+        [$eenheidsprijsCol, $eenheidsprijsRow] = explode("-", $_POST["eenheidsprijs"]);
+
+        if (isset($_POST["totaal"])) {
+            [$totaalCol, $totaalRow] = explode("-", $_POST["totaal"]);
+        }
+
+        $spred = IOFactory::load($_FILES["select_excel"]["tmp_name"]);
+        $shet = $spred->getSheetByName("Offerte klant");
+
+        $arr = [];
+
+        for($i=1;$i<5000;$i++) {
+            $row = $hoeveelheidRow + $i;
+
+            $hoeveelheidVal = $shet->getCellByColumnAndRow($hoeveelheidCol + 1, $row)->getCalculatedValue();
+            $eenheidsprijs = $shet->getCellByColumnAndRow($eenheidsprijsCol + 1, $row)->getCalculatedValue();
+
+
+            // EX; A1:A6
+            $range = PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($artnummerCol + 1) . $row . ":" . PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($beschrijvingCol + 1) . $row;
+
+            $artnbeschrange = $shet->rangeToArray(
+                $range,     // The worksheet range that we want to retrieve
+                NULL,        // Value that should be returned for empty cells
+                TRUE,        // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
+                FALSE,        // Should values be formatted (the equivalent of getFormattedValue() for each cell)
+                FALSE       // Should the array be indexed by cell row and cell column
+            );
+
+            // Remove NULL from array
+            $filteredArray = array_filter($artnbeschrange[0]);
+            
+            $artnummerBeschrijving = implode(" ", $filteredArray);
+            // Eventueel  "&& count($filteredArray) > 1" toevoegen in de if
+            if ($artnummerBeschrijving == "Opmerkingen")
+            {
+                break;
+            }
+            if (!empty($filteredArray)) {
+                error_log($range);
+                error_log($artnummerBeschrijving);
+
+                        // check if "hoeveelheid" has a value
+                if ($hoeveelheidVal != "" && $eenheidsprijs != "") {
+
+                    // Row v/d Vorderingstaat
+                    $curRow = count($arr) + 5;
+
+                    $artnummer = $shet->getCellByColumnAndRow($artnummerCol + 1, $row)->getCalculatedValue();
+
+                    // $eenheidsprijs = $shet->getCellByColumnAndRow($eenheidsprijsCol + 1, $row)->getCalculatedValue();
+
+                    $totaal = "=IFERROR(\$B${curRow}*\$D${curRow},\"\")";
+                    if (isset($totaalCol) && !is_numeric($shet->getCellByColumnAndRow($totaalCol + 1, $row)->getCalculatedValue())) {
+                        // laat cell leeg wanneer bv: Vervalt of Ten laste bouwheer
+                        $totaal = NULL;
+
+                        // Vult Totaal in wanneer bv: Vervalt of Ten laste bouwheer
+                        // $totaal = $shet->getCellByColumnAndRow($totaalCol + 1, $row)->getCalculatedValue();
+                    }
+            //  add value to the array
+                    array_push($arr, [$artnummerBeschrijving, $hoeveelheidVal, $shet->getCellByColumnAndRow($eenheidCol + 1, $row)->getCalculatedValue(), $eenheidsprijs, $totaal,
+                     0, "=\$C" . $curRow, "=IF(ISBLANK(OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),0,-2)),\"\",ROUND(OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),0,-2)*INDIRECT(ADDRESS(ROW(),COLUMN(D9000))),2))", "=OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),,-3)", "=\$C" . $curRow, "=IF(ISBLANK(OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),0,-2)),\"\",ROUND(OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),0,-2)*INDIRECT(ADDRESS(ROW(),COLUMN(D9000))),2))", NULL, "=ROUND(INDIRECT(ADDRESS(ROW(),COLUMN(E9000)))-OFFSET(INDIRECT(ADDRESS(ROW(),COLUMN())),,-2),2)"]);
+                } else {
+                    // Comment out to get only the values that have a quantity and a unitprice
+                    array_push($arr, [$artnummerBeschrijving]);
+                    continue;
+                }
+
+            }
+        }
+
+//        load spreadsheet
+        $reader = IOFactory::createReader('Xlsx');
+        $spreadsheet = $reader->load("template.xlsx");
+        $worksheet = $spreadsheet->getActiveSheet();
+
+
+//        TODO insert length of count($arr) columns before A6
+        $worksheet->insertNewRowBefore(7, count($arr));
+
+////        TODO add range of cells from array $arr to A5 $newsheet->fromArray($arr, NULL, "A5")
+        $worksheet->fromArray($arr, null, "A5", true);
+
+        $date = new DateTime('now');
+        $enddate = $date->modify('last day of this month');
+        
+        $worksheet->setCellValue('F3', $date->format('01/m/Y') . ' t/m ' . $enddate->format('d/m/Y'));
+        $worksheet->setCellValue('I3', 't/m ' . $enddate->format('d/m/Y'));
+
+////        TODO save new document
+        $downwriter = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $downwriter->save("result.xlsx");
+
+        $message = '<script>window.location.href = "/result.xlsx"</script>';
+        
+
+    } else {
+
+        $allowed_extension = array('xls', 'xlsx');
+        $file_array = explode(".", $_FILES['select_excel']['name']);
+        $file_extension = end($file_array);
+        if (in_array($file_extension, $allowed_extension)) {
+            $reader = IOFactory::createReader('Xlsx');
+            if ($_POST["sheet"] != '') {
+                $reader->setLoadSheetsOnly([$_POST["sheet"]]);
+            } else {
+                $reader->setLoadSheetsOnly(["Offerte klant"]);
+            }
+            $spreadsheet = $reader->load($_FILES['select_excel']['tmp_name']);
+            $writer = IOFactory::createWriter($spreadsheet, 'Html');
+            $message = $writer->save('php://output');
+        } else {
+            $message = '<div class="alert alert-danger">Only .xls or .xlsx file allowed</div>';
+        }
+    }
+}
+else
+{
+ $message = '<div class="alert alert-danger">Please Select File</div>';
+}
+
+echo $message;
+
+
+?>
